96666aa81b34f2828e0aedc413a87a2351e4f218
Joshua Erney <57075505+JoshuaErney@users.noreply.github.com>
2025-03-20 13:42:36 -0400

    refactor: remove unused README and responsive CSS, update main.css and index.html for Tic-Tac-Toe game

--- js/main.js
+++ js/main.js
@@ -1,35 +1,26 @@
+const gameBoard = {
+    gameBoard: [],
+}
 
-function checkVictory() { }
+function createPlayer(name, symbol, turn = false) {
+    return { name, symbol, turn };
+}
+
+let playerOne = createPlayer('playerOne', 'X', true);
+let playerTwo = createPlayer('playerTwo', 'O');
+
+function createGameBoard() { }
+
+// creates a 3 by 3 grid of arrays using columns and rows.
+
+function checkVictory() { };
 
 // check to see if the square they click is empty or not, 
 // if empty, it will fill in that square with their "symbol"
 
-function initializeGame() {
-    createGameBoard();
+function gameReset() { };
 
-}
+// Use array.length = 0, to reset each array. 
+// I'll need to use forEach to navigate through each array. 
 
-function createGameBoard() {
-    return {
-        row1: [[], [], []],
-        row2: [[], [], []],
-        row3: [[], [], []],
-    }
-}
-
-function gameReset() {
-    const gameBoard = createGameBoard();
-    Object.values(gameBoard).forEach(row => {
-        row.forEach(cell => cell.length = 0);
-    });
-    return gameBoard;
-};
-
-function createPlayer(name, symbol) {
-    return {
-        name: name,
-        gameWins: 0,
-        gameLosses: 0,
-        symbol: symbol,
-    }
-}
+function takeTurn() { };
