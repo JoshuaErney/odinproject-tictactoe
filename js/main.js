ac81f6a7b5bba66717631c16d0822f41ebd1fec4
Joshua Erney <57075505+JoshuaErney@users.noreply.github.com>
2025-03-25 14:22:51 -0400

    feat: add favicon and web manifest for improved site branding

--- js/main.js
+++ js/main.js
@@ -1,31 +1,26 @@
+const btnStart = document.querySelector("#startGame");
+
 const gameBoard = {
+    // creates a 3 by 3 grid of arrays using columns and rows.
+    createGameBoard() { },
 
+    // Use array.length = 0, to reset each array. 
+    // I'll need to use forEach to navigate through each array.
+    resetGameBoard() { },
 }
 
-function createPlayer(name, symbol, turn = false) {
-    return { name, symbol, turn };
+const Players = {
+
+    createPlayer(name, symbol, turn = false) {
+        return { name, symbol, turn };
+    },
+
+    takeTurn() { },
+
+    // check to see if the square they click is empty or not, 
+    // if empty, it will fill in that square with their "symbol"
+    checkVictory() { },
 }
 
-let playerOne = createPlayer('playerOne', 'X', true);
-let playerTwo = createPlayer('playerTwo', 'O');
-
-let columns = 3;
-let rows = 3;
-
-function createGameBoard(columns, rows) {
-
-}
-
-// creates a 3 by 3 grid of arrays using columns and rows.
-
-function checkVictory() { };
-
-// check to see if the square they click is empty or not, 
-// if empty, it will fill in that square with their "symbol"
-
-function gameReset() { };
-
-// Use array.length = 0, to reset each array. 
-// I'll need to use forEach to navigate through each array. 
-
-function takeTurn() { };
+let playerOne = gameBoard.createPlayer('playerOne', 'X', true);
+let playerTwo = gameBoard.createPlayer('playerTwo', 'O');
